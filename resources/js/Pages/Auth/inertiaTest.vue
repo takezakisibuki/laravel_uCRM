7b1a8f991c2948ac662a7f1f63ae488e3cea703b
takezakisibuki <kaizyudebugon@gmail.com>
2024-04-06 23:36:18 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Auth/inertiaTest.vue
@@ -0,0 +1,25 @@
+<script  setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue';
+
+const newTitle = ref('');
+const newContent = ref('');
+</script>
+
+<template>
+    <p>inertiaのテストです。</p>
+    <br>
+    <a href="/">aタグ経由です</a><br>
+    <Link href="/">link経由です</Link><br>
+    <Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
+    <Link :href="route('inertia.show',{id:1})">ルートパラメータのテストです。</Link><br>
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle" placeholder="メッセージを入力してください">{{}}<br>
+    <input type="text" name="newTitle" v-model="newContent" placeholder="メッセージを入力してください"><br>
+    <Link as="button" :href="route('inertia.store')" method="post" 
+    :data="{
+        title:newTitle,
+        content:newContent
+        }">DB保存テスト</Link>
+
+</template>
---
+++ resources/js/Pages/Inertia/componentTest.vue
@@ -0,0 +1,16 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from "@/Components/InputLabel.vue";
+import Input from "@/Components/TextInput.vue";
+
+const emittest = (e) => {
+    console.log(e);
+}
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名"></Label>
+        <Input modelValue="初期値が入ります。" @update:modelValue="emittest"></Input>
+    </GuestLayout>
+</template>
